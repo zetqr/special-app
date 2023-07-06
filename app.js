85a711c1911e7356a75163e36cd96ba2540ad6fd
farneil <mkalandaris@gmail.com>
2023-07-06 16:38:01 +0400

    error code added

--- app.js
+++ app.js
@@ -2,4 +2,5 @@ $(function(){
     let button = document.querySelector('.submit');
     let pricesBlock = document.querySelector('.prices');
     pricesBlock.style.backgroundColor = 'green';
+    error code:
 });
